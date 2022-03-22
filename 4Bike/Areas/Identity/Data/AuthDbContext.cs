8a9a674ba35a86546dc019596f9a32e1434a9789
pblomqvist <93123616+pblomqvist@users.noreply.github.com>
2022-03-22 11:03:28 +0100

    Implemented identity function, working on roles

---
+++ 4Bike/Areas/Identity/Data/AuthDbContext.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using _4Bike.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace _4Bike.Data
+{
+    public class AuthDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public AuthDbContext(DbContextOptions<AuthDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+
+            //User roles
+            string roleId = Guid.NewGuid().ToString();
+            string userRoleId = Guid.NewGuid().ToString();
+            string userId = Guid.NewGuid().ToString();
+
+            builder.Entity<IdentityRole>().HasData(new IdentityRole
+            {
+                Id = roleId,
+                Name = "Admin",
+                NormalizedName = "ADMIN"
+            });
+
+            builder.Entity<IdentityRole>().HasData(new IdentityRole
+            {
+                Id = userId,
+                Name = "User",
+                NormalizedName = "USER"
+            });
+
+            //Make pw hasher for seeded users
+            PasswordHasher<ApplicationUser> hasher = new PasswordHasher<ApplicationUser>();
+
+            //Seeded users
+            builder.Entity<ApplicationUser>().HasData(new ApplicationUser
+            {
+                Id = userId,
+                Email = "admin@admin.com",
+                NormalizedEmail = "ADMIN@ADMIN.COM",
+                UserName = "admin@admin.com",
+                NormalizedUserName = "ADMIN@ADMIN.COM",
+                PasswordHash = hasher.HashPassword(null, "password"),
+                FirstName = "Admin",
+                LastName = "Adminsson"
+            });
+
+            builder.Entity<IdentityUserRole<string>>().HasData(new IdentityUserRole<string>
+            {
+                RoleId = roleId,
+                UserId = userId
+            });
+
+        }
+
+        public DbSet<ApplicationUser> Users { get; set; }
+
+    }
+}
---
+++ 4Bike/Controllers/RoleController.cs
@@ -0,0 +1,27 @@
+﻿using _4Bike.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _4Bike.Controllers
+{
+    public class RoleController : Controller
+    {
+        //private readonly RoleManager<IdentityRole> _roleManager;
+        //private readonly UserManager<ApplicationUser> _userManager;
+
+        //public RoleController(RoleManager<IdentityRole> roleManager, UserManager<ApplicationUser> userManager)
+        //{
+        //    _roleManager = roleManager;
+        //    _userManager = userManager;
+        //}
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
