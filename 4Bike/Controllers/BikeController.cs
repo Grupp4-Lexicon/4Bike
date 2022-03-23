7922691a1d4642b32c77505b8a54cdc3be1ba4e5
SmolSwede <samuel_schon@hotmail.com>
2022-03-23 10:36:17 +0100

    Added Maryams Views, ViewModels and Controler for Bike. And fixed the link between controller and view for AddBike

---
+++ 4Bike/Controllers/BikeController.cs
@@ -0,0 +1,62 @@
+﻿using _4Bike.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using _4Bike.Models.Products;
+using _4Bike.Models.ViewModels;
+
+
+namespace _4Bike.Controllers
+{
+    public class BikeController : Controller
+    {
+        private readonly AuthDbContext _context;
+
+        public BikeController(AuthDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ListBikes()
+        {
+            var bikesInfo = _context.Bikes.ToList();
+
+            return View(bikesInfo);
+        }
+
+        public IActionResult AddBike()
+        {
+            ViewBag.Bikes = _context.Bikes.ToList();
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddBike(AddBikesViewModel bike)
+        {
+            if (ModelState.IsValid)
+            {
+                Product_Bike bikeProduct = new Product_Bike
+                {
+                    BikeName = bike.BikeName,
+                    BikePrice = bike.Price,
+                    BikePicNav = bike.Pic,
+
+
+                };
+
+                _context.Bikes.Add(bikeProduct);
+                _context.SaveChanges();
+
+                Product_Bike insertedBike = _context.Bikes.FirstOrDefault(a => a.BikeName == bikeProduct.BikeName);
+            }
+            return RedirectToAction("ListBikes");
+        }
+    }
+}
