c238df8dcece23c81bbd393536cab520787c93ff
pblomqvist <93123616+pblomqvist@users.noreply.github.com>
2022-03-24 09:56:20 +0100

    Implemented functionality to edit user info

---
+++ 4Bike/Models/ViewModels/EditUserViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _4Bike.Models.ViewModels
+{
+    public class EditUserViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [StringLength(100, MinimumLength = 1)]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "First name")]
+        [StringLength(100, MinimumLength = 1)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last name")]
+        [StringLength(100, MinimumLength = 1)]
+        public string LastName { get; set; }
+
+        [Required]
+        [Display(Name = "Address")]
+        [StringLength(100, MinimumLength = 1)]
+        public string Address { get; set; }
+
+        public EditUserViewModel() { }
+
+        public EditUserViewModel(string email, string fName, string lName, string address)
+        {
+            Email = email;
+            FirstName = fName;
+            LastName = lName;
+            Address = address;
+        }
+    }
+}
